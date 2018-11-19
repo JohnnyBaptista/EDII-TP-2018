2791ca67f276599a0977927241aa939cfd6067b9
JohnnyBaptista <joaosbaptista@hotmail.com>
2018-11-19 17:53:01 -0200

    Correções e implementações

--- main.c
+++ main.c
@@ -4,18 +4,22 @@
 
 int main(){
 	
-	FILE *fp, *fp2, *fb1;
+	FILE *fp, *fp2, *fp3; //texto 
+	FILE *fb1, *fb2, *fb3; //binario
 	int i = 0, j = 0;
 	char grau;
 	char pos;
 	char txt1[10], txt2[10];
+	DADOS *cidades[24];
+	DADOS *cidades2[24];
 	arvoreB *arv[8]; // 8 = grau máximo da árvore
 	arvoreB *arv2[8]; // 8 = grau máximo da árvore; auxiliar
 	//==========================================================||==========================================================\\
 
 	fp = fopen("arq/config.txt", "r");
 	configRead(fp, &grau, &pos, txt1, txt2);
-	printf("%d\n", pos);
+	fp2 = fopen("arq/texto1.txt", "r");
+	fp3 = fopen("arq/texto2.txt", "r");
 	//iniciando arvore;
 	/*for (; i < 8; ++i){
 		arv[i]->isFolha = FALSE;
@@ -29,14 +33,27 @@ int main(){
 	
 	printf("Grau = %c\nPos = %c\nArq1 = %s\nArq2 = %s\n", grau, pos, txt1, txt2);
 
-	fp2 = fopen(txt1, "r");
-	fb1 = fopen("arq/binary1.txt", "wb+");
-	text1Read(fp2, arv);
-	/*//printStruct(arv);
-	binary1Create(arv, fb1);
+
+	// text1Read(fp2, arv);
+	text2Read(fp3, cidades);
+	// printStruct2(cidades);
+	fb2 = fopen("arq/binary2.txt", "wb+");
+	boolean test = binary2Create(cidades, fb2);
+	fflush(fb2);
+	fb2 = fopen("arq/binary2.txt","rb");
+	binaryRead2(fb2, cidades2);
+
+
+	// printStruct(arv);
+	// printf("IMPRIMINDO ARQUIVO BINARIO\n=================\n\n");
+	// fb1 = fopen("arq/binary1.txt", "wb+");
+	// binary1Create(arv, fb1);
+	/*//
 	// printf("%d\n", test);
-	fb1 = fopen("arq/binary1.txt", "rb");
-	int test = binaryRead(fb1, arv2);	*/
+	*/
+	// fflush(fb1);
+	// fb1 = fopen("arq/binary1.txt", "rb");
+	// int test = binaryRead(fb1, arv2);	
 	// printf("%d\n", test);
 	return 0;
 }
--- oper.h
+++ oper.h
@@ -3,8 +3,10 @@
 
 void printStruct(arvoreB *arv[]);
 boolean configRead(FILE *fp, char *grau, char *pos, char *txt1, char *txt2);
-int text1Read(FILE *fp, arvoreB *arv[]);
-int binary1Create(arvoreB **arv, FILE *fb);
-int binaryRead(FILE *fb, arvoreB **arv);
+boolean text1Read(FILE *fp, arvoreB *arv[]);
+boolean binary1Create(arvoreB **arv, FILE *fb);
+boolean binaryRead(FILE *fb, arvoreB **arv);
+boolean text2Read(FILE *fp, DADOS *cidades[]);
+boolean binary2Create(DADOS *cidades[], FILE *fp);
 
 #endif
