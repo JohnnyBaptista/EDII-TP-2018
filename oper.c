a0165ba5b07c720eca1d68e0a7a4d0b1140f97c7
JohnnyBaptista <joaosbaptista@hotmail.com>
2018-11-19 08:54:23 -0200

    Correções finais da branch
    Coloquei todo o arq1 na estutura

--- oper.c
+++ oper.c
@@ -17,30 +17,37 @@ boolean configRead(FILE *fp, char *grau, char *pos, char *txt1, char *txt2){
 	
 }
 
-int text1Read(FILE *fp, arvoreB arv[]){ // coloca o arquivo de texto1 na struct
+int text1Read(FILE *fp, arvoreB *arv[]){ // coloca o arquivo de texto1 na struct
+	printf("entrou na função\n");
 	int aux, i = 0, j = 0;
 	if (fp == NULL) printf("ERRO AO LER ARQUIVO!!\n");
 	else{
 		do{
-			arvoreB novoNo;
+			arvoreB *novoNo = (arvoreB *) malloc(sizeof(arvoreB));
 			//lendo primeiro a estrutura da arvore B+
-			fscanf(fp, "%d %d", novoNo.numChaves, aux);
-			if (aux == 1) novoNo.isFolha = TRUE;
-			else novoNo.isFolha = FALSE;
+			fscanf(fp, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", 
+												&novoNo->numChaves, 
+												&aux, 
+												&novoNo->chaves[0],
+												&novoNo->chaves[1],
+												&novoNo->chaves[2],
+												&novoNo->chaves[3],
+												&novoNo->chaves[4],
+												&novoNo->chaves[5],
+												&novoNo->chaves[6],
+												&novoNo->filhos[0],
+												&novoNo->filhos[1],
+												&novoNo->filhos[2],
+												&novoNo->filhos[3],
+												&novoNo->filhos[4],
+												&novoNo->filhos[5],
+												&novoNo->filhos[6],
+												&novoNo->filhos[7]);
+			if (aux == 1) novoNo->isFolha = TRUE;
+			else novoNo->isFolha = FALSE;
 			//lendo as chaves
-			for(; i < MAX_CHAVES; i++)
-				fscanf(fp, "%d", novoNo.chaves[i]);
-			//lendo os filhos / indices
-			for(i = 0; i < MAX_FILHOS; i++)
-				fscanf(fp, "%d", novoNo.filhos[i]);
 
-			// fseek(fp, 1, SEEK_CUR);
-			// fscanf(fp, "%d", aux);
-			// if (aux == 0) arv->isFolha = FALSE;
-			// else arv->isFolha = TRUE;
-			// for (int i = 0; i < ; ++i){
-			// 	/* code */
-			// }
+		
 			arv[j] = novoNo;
 			j++;
 		}while(!feof(fp));
---
+++ oper.h
@@ -0,0 +1,7 @@
+#ifndef _OPER_H_
+#define _OPER_H_
+
+boolean configRead(FILE *fp, char *grau, char *pos, char *txt1, char *txt2);
+int text1Read(FILE *fp, arvoreB *arv[]);
+
+#endif
