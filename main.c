c8fb13e2d731da7752177b25b51d8a6a4cd89e9f
JohnnyBaptista <joaosbaptista@hotmail.com>
2018-11-19 02:14:23 -0200

    Atualização
    Atualização do arquivo txt e inicio da implementação na struct

--- main.c
+++ main.c
@@ -4,15 +4,29 @@
 
 int main(){
 	
-	FILE *fp;
+	FILE *fp, *fp2;
 
 	fp = fopen("arq/config.txt", "r");
+	fp2 = fopen("arq/texto1.txt", "r");
+	int i = 0, j = 0;
 	char grau;
 	char pos;
 	char txt1[10], txt2[10];
-	
+	arvoreB arv[8];
 	configRead(fp, &grau, &pos, txt1, txt2);
 
+	//iniciando arvore;
+	for (; i < 8; ++i){
+		arv[i].isFolha = FALSE;
+		arv[i].numChaves = 0;
+		for(; j < MAX_CHAVES; j++)
+			arv[i].chaves[j] = 0;
+		for(j = 0; j < MAX_FILHOS; j++)
+			arv[i].filhos[j] = 0;
+	}
+
+	text1Read(fp2, arv);
+	printf("%d %d %d\n", arv[0].numChaves, arv[0].chaves[0], arv[0].filhos[0]);
 	printf("Grau = %c\nPos = %c\nArq1 = %s\nArq2 = %s", grau, pos, txt1, txt2);
 
 
--- oper.c
+++ oper.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structures.h"
+#include "oper.h"
 
 //arquivo f deve estar aberto já
-int configRead(FILE *fp, char grau, char pos, char *txt1, char *txt2){
+boolean configRead(FILE *fp, char *grau, char *pos, char *txt1, char *txt2){
 	int i = 0;
-	grau = fgetc(fp);
-	fseek(fp, 1, SEEK_CUR);
-	pos = fgetc(fp);
-	fscanf(fp, "%s", txt1);
-	fscanf(fp, "%s", txt2);
+	if (fp != NULL){
+		fscanf(fp, "%c", grau);
+		fseek(fp, 1, SEEK_CUR);
+		fscanf(fp, "%c", pos);
+		fscanf(fp, "%s", txt1);
+		fscanf(fp, "%s", txt2);
+		return TRUE;
+	} else return FALSE;
+	
+}
+
+int text1Read(FILE *fp, arvoreB arv[]){ // coloca o arquivo de texto1 na struct
+	int aux, i = 0, j = 0;
+	if (fp == NULL) printf("ERRO AO LER ARQUIVO!!\n");
+	else{
+		do{
+			arvoreB novoNo;
+			//lendo primeiro a estrutura da arvore B+
+			fscanf(fp, "%d %d", novoNo.numChaves, aux);
+			if (aux == 1) novoNo.isFolha = TRUE;
+			else novoNo.isFolha = FALSE;
+			//lendo as chaves
+			for(; i < MAX_CHAVES; i++)
+				fscanf(fp, "%d", novoNo.chaves[i]);
+			//lendo os filhos / indices
+			for(i = 0; i < MAX_FILHOS; i++)
+				fscanf(fp, "%d", novoNo.filhos[i]);
+
+			// fseek(fp, 1, SEEK_CUR);
+			// fscanf(fp, "%d", aux);
+			// if (aux == 0) arv->isFolha = FALSE;
+			// else arv->isFolha = TRUE;
+			// for (int i = 0; i < ; ++i){
+			// 	/* code */
+			// }
+			arv[j] = novoNo;
+			j++;
+		}while(!feof(fp));
+		return 1;
+	}
+	return 0;
 }
